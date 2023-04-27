5de16b1d33c8c028b79e2d45df07eff5ee15e4d4
Ixchel García <ixcheldelsun@gmail.com>
2023-04-27 13:13:09 -0600

    answer to any phone number

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Body, Request
+import uvicorn
+import re
 
 load_dotenv()
 
@@ -9,37 +11,25 @@ account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
-def send_message(body_text):
+def send_message(body_text, to_number=None):
+    number = '+' + str(re.findall(r'\d+', to_number)[0])
     client.messages.create(
-        from_="whatsapp:+14155238886", body=body_text, to="whatsapp:+584122370830"
+        from_="whatsapp:+14155238886", body=body_text, to=f"whatsapp:{number}"
     )
 
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
 @app.post("/message")
-def init_bot(Body: str = Form()):
-    message = Body.lower() # get the message the user sent our bot
-    response = f"Hello, I am a bot. I am still learning. I got the following message from you: {str(message)}."
-    # user_msg = request.values.get('Body', '').lower()
- 
-    # # creating object of MessagingResponse
-    # response = MessagingResponse()
- 
-    # # User Query
-    # q = user_msg + "geeksforgeeks.org"
- 
-    # # list to store urls
-    # result = []
- 
-    # # searching and storing urls
-    # for i in search(q, tld='co.in', num=6, stop=6, pause=2):
-    #     result.append(i)
- 
-    # # displaying result
-    # msg = response.message(f"--- Results for '{user_msg}' ---")
-    # for result in search_results:
-    #     msg = response.message(result)
- 
-    return send_message(response)
+async def init_bot(From: str = Form(...), Body: str = Form(...) ):
+    response = f"Hello, I am a bot. I am still learning. I got the following message from you: {str(Body)}."
+
+    return send_message(response, From)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
